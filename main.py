4335ad4ff6dc654e55f31a614de25113842ba1bb
Vlad <djmix2000@yandex.ru>
2025-05-19 15:03:21 +0300

    Init main.py

---
+++ main.py
@@ -0,0 +1,55 @@
+import asyncio
+import subprocess
+import aiohttp
+
+
+async def check_single_domain(subdomain:str)->str:
+    async with aiohttp.ClientSession() as session:
+            try:
+                async with session.get(f'https://{subdomain}', ssl=False) as response:
+                    if response.status == 200:
+                        return subdomain
+            except Exception as e:
+                print(f"[!] Error for {subdomain}: {e}")
+
+
+
+class Recon:
+    def __init__(self,domain:str):
+        self.subdomain_list: list[str] = []
+        self.domain = domain
+        self.gau_list: list[list[str]]=[]
+
+    async def check_subdomain(self):
+        tasks = [check_single_domain(subdomain) for subdomain in self.subdomain_list]
+        results = await asyncio.gather(*tasks)
+        self.subdomain_list = [result for result in results if result is not None]
+
+
+    async def subdomain_parse(self):
+        subfinder = subprocess.run(["subfinder", "-d", f"{self.domain}"], capture_output=True, text=True)
+        self.subdomain_list = subfinder.stdout.split('\n')
+        await self.check_subdomain()
+
+    def gau_parse(self):
+        for domain in self.subdomain_list:
+            gau = subprocess.run(['gau',f"{domain}"],capture_output=True,text=True)
+            gau_list_temp = gau.stdout.split('\n')
+            self.gau_list.append(gau_list_temp)
+
+
+
+
+
+
+async def main():
+    kuper = Recon("kuper.ru")
+    await kuper.subdomain_parse()
+    kuper.gau_parse()
+    print(kuper.gau_list)
+
+
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
